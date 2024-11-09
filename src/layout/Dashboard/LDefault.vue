9fa848902f77ce9502acfef91f7ff9466b417704
hamrayev <a.xamroyev@imkon.uz>
2024-11-09 13:18:40 +0500

    fix: fix

--- src/layout/Dashboard/LDefault.vue
+++ src/layout/Dashboard/LDefault.vue
@@ -2,8 +2,6 @@
   <div class="bg-[#F5F6F7] min-h-screen flex">
 
     <div class="w-full h-full">
-      <CHeader v-bind="{ activeRoute }" />
-
       <div class="h-full w-full p-8">
         <div class="w-full h-full">
           <slot />
@@ -13,7 +11,4 @@
   </div>
 </template>
 <script lang="ts" setup>
-import CHeader from "@/layout/Dashboard/components/CHeader.vue";
-
-const activeRoute = "/";
 </script>
