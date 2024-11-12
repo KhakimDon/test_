79571010d2cdf6de998fdec5ccfefb9444a93400
x.almamedov <x.almamedov@uic.group>
2024-11-12 12:52:20 +0500

    fix: change

--- src/layout/Dashboard/LDefault.vue
+++ src/layout/Dashboard/LDefault.vue
@@ -4,6 +4,7 @@
     <div class="w-full h-[calc(100vh-36px)] ">
       <div class="h-full w-full">
         <div class="w-full h-full">
+<!--          hello -->
           <slot />
           <Menu/>
         </div>
